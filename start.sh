5a33618d3a0076830da20a9984cbaca1ec0b93f6
Jin Wang <jin@student.unimelb.edu.au>
2015-08-16 22:35:05 +1000

    Whoops. Wrong bash script

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ DEMO_WEB_INDEX_TEMP_FILE="/demo-web/index.html.bak"
 EMBEDDED_JS_URL="http://xlabs-admin.uthoft.com/scripts/xLabsAnalytics.js"
 
 sed -e 's|js/xLabsAnalytics.js|'${EMBEDDED_JS_URL}'|' ${DEMO_WEB_INDEX_FILE} > ${DEMO_WEB_INDEX_TEMP_FILE}
-rm ${DEMO_WEB_INDEX_TEMP_FILE}
+mv ${DEMO_WEB_INDEX_TEMP_FILE} ${DEMO_WEB_INDEX_FILE}
 
 http-server -c-1 -p 80
