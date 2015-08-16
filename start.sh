ed1fdbba67c7f473213d515968e72ace4b2710ac
Jin Wang <jin@student.unimelb.edu.au>
2015-08-17 01:09:26 +1000

    Update the start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 DEMO_WEB_INDEX_FILE="/demo-web/index.html"
 DEMO_WEB_INDEX_TEMP_FILE="/demo-web/index.html.bak"
-EMBEDDED_JS_URL="http://xlabs-admin.uthoft.com/scripts/xLabsAnalytics.js"
+EMBEDDED_JS_URL="http://xlabs-admin.uthoft.com/assets/js/xLabsAnalytics.js"
 
 sed -e 's|js/xLabsAnalytics.js|'${EMBEDDED_JS_URL}'|' ${DEMO_WEB_INDEX_FILE} > ${DEMO_WEB_INDEX_TEMP_FILE}
 mv ${DEMO_WEB_INDEX_TEMP_FILE} ${DEMO_WEB_INDEX_FILE}
