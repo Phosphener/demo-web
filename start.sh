217734201d51e17328cd29a0a2c541258476fac6
Jin Wang <jin@student.unimelb.edu.au>
2015-08-16 22:00:01 +1000

    Change the remote embedded js to local one

--- start.sh
+++ start.sh
@@ -1,1 +1,8 @@
+DEMO_WEB_INDEX_FILE="/demo-web/index.html"
+DEMO_WEB_INDEX_TEMP_FILE="/demo-web/index.html"
+EMBEDDED_JS_URL="http://xlabs-admin.uthoft.com/scripts/xLabsAnalytics.js"
+
+sed -e 's|js/xLabsAnalytics.js|'${EMBEDDED_JS_URL}'|' ${DEMO_WEB_INDEX_FILE} > ${DEMO_WEB_INDEX_TEMP_FILE}
+rm ${DEMO_WEB_INDEX_TEMP_FILE}
+
 http-server -c-1 -p 80
